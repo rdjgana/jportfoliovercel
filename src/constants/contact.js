584f439f8a3177dce08f4f42c5b0c9121225cbd5
Dinesh-74 <your.email@example.com>
2026-06-29 19:16:13 +0530

    change  mail id

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,2 +1,2 @@
-export const CONTACT_EMAIL = 'jacifelix@icloud.com';
+export const CONTACT_EMAIL = 'contact@jacifelix.com';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/jaci-felix-8bb17833a';
